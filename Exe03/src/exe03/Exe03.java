5922adf0662d9dbc47ba5f307189508bc185bdac
FelipeAN26 <54293258+FelipeAN26@users.noreply.github.com>
2019-08-19 20:32:41 -0300

    Inicialização Projeto03Casting-Java

---
+++ Exe03/src/exe03/Exe03.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe03;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exe03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double pi = 3.14;
+        int i = (int) pi; // casting = se molde a...
+        System.out.println("Valor de i : " + i);
+        System.out.println("Valor de Pi : " + pi);
+        
+        float j = 3.14f; // Quando se tem valores decimais, colocar ' f '
+        float k = 3; // sem o 'f' quando nao exister valor decimal
+        float m =  (float) pi; //casting
+    }
+    
+}
